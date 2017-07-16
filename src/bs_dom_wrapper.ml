048191a4097735603ee0f95094c2abde46213ff5
derui <derutakayu@gmail.com>
2017-07-17 08:36:16 +0900

    Add DOMRect binding

--- src/bs_dom_wrapper.ml
+++ src/bs_dom_wrapper.ml
@@ -9,6 +9,7 @@ module File_reader = Bs_dom_wrapper_file_reader
 module Form_data = Bs_dom_wrapper_form_data
 module Data_transfer = Bs_dom_wrapper_data_transfer
 module Css_style_declaration = Bs_dom_wrapper_css_style_declaration
+module Dom_rect = Bs_dom_wrapper_dom_rect
 
 (* binding for global variable *)
 external document: Dom.document = "" [@@bs.val]
