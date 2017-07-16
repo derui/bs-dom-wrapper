95f30e38b76b660bf1ba15799be7a441e2d2c44c
derui <derutakayu@gmail.com>
2017-07-16 16:47:28 +0900

    Add CSSStyleDeclaration

--- src/bs_dom_wrapper.ml
+++ src/bs_dom_wrapper.ml
@@ -8,6 +8,7 @@ module File = Bs_dom_wrapper_file
 module File_reader = Bs_dom_wrapper_file_reader
 module Form_data = Bs_dom_wrapper_form_data
 module Data_transfer = Bs_dom_wrapper_data_transfer
+module Css_style_declaration = Bs_dom_wrapper_css_style_declaration
 
 (* binding for global variable *)
 external document: Dom.document = "" [@@bs.val]
