9198fd27b70c5ebc9b619909a4045c6bcc67aea9
derui <derutakayu@gmail.com>
2017-07-16 10:28:46 +0900

    Fix to add property name for bs.set

--- src/bs_dom_wrapper_nodes_document.ml
+++ src/bs_dom_wrapper_nodes_document.ml
@@ -17,33 +17,33 @@ module Make(T:TYPE) = struct
   external lastStyleSheetSet: T.t -> string = "" [@@bs.get]
   external preferredStyleSheetSet: T.t -> string = "" [@@bs.get]
   external selectStyleSheetSet: T.t -> string = "" [@@bs.get]
-  external setSelectStyleSheetSet: T.t -> string -> unit = "" [@@bs.set]
+  external setSelectStyleSheetSet: T.t -> string -> unit = "selectStyleSheetSet" [@@bs.set]
   external styleSheets: T.t -> Dom.cssStyleSheet array = "" [@@bs.get]
-  external styleSheetSets: T.t -> string array = "" [@@bs.get]
+  external styleSheetSets: T.t -> string array = "styleSheets" [@@bs.get]
   external visibilityState: T.t -> string = "" [@@bs.get]
   let visibilityState t =
     visibilityState t |> Types.Visibility_state.of_string
 
   external activeElement: T.t -> Dom.element option = "" [@@bs.get] [@@bs.return null_to_opt]
   external body: T.t -> Dom.element = "" [@@bs.get]
-  external setBody: T.t -> Dom.element -> unit = "" [@@bs.set]
+  external setBody: T.t -> Dom.element -> unit = "body" [@@bs.set]
   external cookie: T.t -> string = "" [@@bs.get]
-  external setCookie: T.t -> string -> unit = "" [@@bs.set]
+  external setCookie: T.t -> string -> unit = "cookie" [@@bs.set]
   external defaultView: T.t -> Dom.window = "" [@@bs.get]
   external designMode: T.t -> string = "" [@@bs.get]
   let designMode t = designMode t |> Types.Design_mode.of_string
 
-  external setDesignMode: T.t -> string -> unit = "" [@@bs.set]
+  external setDesignMode: T.t -> string -> unit = "designMode" [@@bs.set]
   let setDesignMode t v =
     let mode = Types.Design_mode.to_string v in setDesignMode t mode
 
   external dir: T.t -> string = "" [@@bs.get]
   let dir t = dir t |> Types.Dir.of_string
-  external setDir: T.t -> string -> unit = "" [@@bs.set]
+  external setDir: T.t -> string -> unit = "dir" [@@bs.set]
   let setDir t v = let v = Types.Dir.to_string v in setDir t v
 
   external domain: T.t -> string = "" [@@bs.get]
-  external setDomain: T.t -> string -> unit = "" [@@bs.set]
+  external setDomain: T.t -> string -> unit = "domain" [@@bs.set]
 
   external embeds: T.t -> Dom.htmlCollection = "" [@@bs.get]
   external forms: T.t -> Dom.htmlCollection = "" [@@bs.get]
@@ -58,8 +58,8 @@ module Make(T:TYPE) = struct
 
   external referrer: T.t -> string = "" [@@bs.get]
   external title: T.t -> string = "" [@@bs.get]
-  external setTitle: T.t -> string -> unit = "" [@@bs.set]
-  external url: T.t -> string = "" [@@bs.get]                                   
+  external setTitle: T.t -> string -> unit = "title" [@@bs.set]
+  external url: T.t -> string = "" [@@bs.get]
 
   (* methods *)
   external adoptNode: 'a Dom.node_like -> unit = "" [@@bs.send.pipe:T.t]
