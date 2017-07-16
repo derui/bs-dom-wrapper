97bb2e8d05fcdb6773b7dca4987975b2934926e9
derui <derutakayu@gmail.com>
2017-07-16 11:24:05 +0900

    Rename method for binding

--- src/bs_dom_wrapper_nodes_event_target.ml
+++ src/bs_dom_wrapper_nodes_event_target.ml
@@ -7,11 +7,11 @@ end
 
 (* Make Event_target interface.  *)
 module Make(T:TYPE) = struct
-  external as_event_target: T.t -> Dom.eventTarget = "%identity"
+  external asEventTarget: T.t -> Dom.eventTarget = "%identity"
 
-  external add_event_listener: string -> (Dom.event -> unit) -> unit = "addEventListener" [@@bs.send.pipe:T.t]
-  external remove_event_listener: string -> (Dom.event -> unit) -> unit = "removeEventListener" [@@bs.send.pipe:T.t]
-  external dispatch_event: Dom.event -> unit = "dispatchEvent" [@@bs.send.pipe:T.t]
+  external addEventListener: string -> (Dom.event -> unit) -> unit = "addEventListener" [@@bs.send.pipe:T.t]
+  external removeEventListener: string -> (Dom.event -> unit) -> unit = "removeEventListener" [@@bs.send.pipe:T.t]
+  external dispatchEvent: Dom.event -> unit = "dispatchEvent" [@@bs.send.pipe:T.t]
 end
 
 type t = Dom.eventTarget
