9e917f503c029976b65d6d3a378ce08a8fae7bbb
derui <derutakayu@gmail.com>
2017-08-02 22:31:04 +0900

    Fix module names

--- src/bs_dom_wrapper.ml
+++ src/bs_dom_wrapper.ml
@@ -1,9 +1,9 @@
 (* Wrapper main *)
 
-module Blob = Bs_file_wrapper_blob
-module File = Bs_file_wrapper_file
-module File_reader = Bs_file_wrapper_file_reader
-module Form_data = Bs_file_wrapper_form_data
+module Blob = Bs_dom_wrapper_blob
+module File = Bs_dom_wrapper_file
+module File_reader = Bs_dom_wrapper_file_reader
+module Form_data = Bs_dom_wrapper_form_data
 module URL = Bs_dom_wrapper_url
 
 (* binding for global variable *)
